15f51b50898a288ecc56b4c6b75b97a885dc54fe
petzenred <43352803+petzenred@users.noreply.github.com>
2025-09-03 11:55:40 -0700

    Renamed several sound files for to be more clear

--- resources/audio/audio_directory.py
+++ resources/audio/audio_directory.py
@@ -5,7 +5,7 @@ from definitions import *
 __all__ = ['MUSIC_PLAYLISTS', 'SOUND_INDEX']
 
 MUSIC_PLAYLISTS: dict = {
-    MAIN_MENU_SCREENS_KEY: ["Forest_Ambiance_1.mp3"],
+    MAIN_MENU_SCREENS_KEY: ["Clangen_Generations_441khz.mp3"],
     CREATION_SCREENS_KEY: ["Clangen_Generations_441khz.mp3"],
     Biome.Beach: ["Beach_Ambiance_1.mp3", "Beach_Ambiance_2.mp3"],
     Biome.Desert: ["Desert_Ambiance_1.mp3"],
@@ -17,12 +17,13 @@ MUSIC_PLAYLISTS: dict = {
 }
 
 SOUND_INDEX: dict = {
-    "button_press": ["Button_Click_4.mp3"],
-    "button_hover": ["Button_Hover_3.mp3"],
+    "button_press": ["Action_Click.mp3"], # used to be "Button_Click_4.mp3"
+    "button_hover": ["Action_Hover.mp3"], # used to be "Button_Hover_3.mp3"
     "page_flip": ["Page_Flip_1.mp3","Page_Flip_2.mp3","Page_Flip_3.mp3","Page_Flip_4.mp3","Page_Flip_5.mp3"],
     "dice_roll": ["Dice_Roll_1.mp3","Dice_Roll_2.mp3","Dice_Roll_3.mp3","Dice_Roll_4.mp3",
                   "Dice_Roll_5.mp3","Dice_Roll_6.mp3","Dice_Roll_7.mp3"],
-    "save": ["Save_Button.mp3"],
-    "timeskip": ["Timeskip_Button.mp3"],
-    "antagonize": ["Antagonize_Button.mp3"]
+    "save": ["Button_Save.mp3"], # used to be "Save_Button.mp3"
+    "timeskip": ["Button_Timeskip.mp3"], # used to be "Timeskip_Button.mp3"
+    "antagonize": ["Button_Antagonize.mp3"], # used to be "Antagonize_Button.mp3"
+    "favourite": ["Button_Favourite.mp3"] # used to be "favorite_cat.mp3"
 }
